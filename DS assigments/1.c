8bdb37b9a7177c8fbeceb375e9d0fc747b03010e
Atul <atul993418@gmail.com>
2025-02-09 17:25:26 +0530

    first commit

---
+++ DS assigments/1.c
@@ -0,0 +1,14 @@
+// Write a program to 
+//  (a.) create an array of integers and initialize it at compile-time
+#include <stdio.h>
+int main(){
+    int array[5] = {1,2,3,4,5}; 
+}
+
+//  (b.) create another array of floating values and initialize it at run-time
+#include <stdio.h>
+int main(){
+
+}
+
+//  (c.) display the elements of both the arrays with proper headings
